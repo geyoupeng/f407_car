1f6da5687537676a8e76afc35b5e699dda6fb33c
xiaoge <1834676571@qq.com>
2023-08-15 09:46:22 +0800

    deleted-v

--- obiect_key_demo/MDK-ARM/obiect_key_demo/key_app.d
+++
@@ -1,7 +0,0 @@
-obiect_key_demo\key_app.o: ..\1_Applications\key_app.c
-obiect_key_demo\key_app.o: ..\2_Devices\devices.h
-obiect_key_demo\key_app.o: ..\3_ModuleDrvs\drivers.h
-obiect_key_demo\key_app.o: ..\4_Includes\config.h
-obiect_key_demo\key_app.o: ..\3_ModuleDrvs\drv_gpio.h
-obiect_key_demo\key_app.o: ..\2_Devices\dev_gpio.h
-obiect_key_demo\key_app.o: ..\4_Includes\my_type.h
--- obiect_key_demo/MDK-ARM/obiect_key_demo/startup_stm32f407xx.d
+++
@@ -1,1 +0,0 @@
-obiect_key_demo\startup_stm32f407xx.o: startup_stm32f407xx.s
